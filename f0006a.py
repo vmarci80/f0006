d9259d7d647577990ef7f87bc781b660a3302aec
vmarci80 <wmtech.vetromarton@gmail.com>
2020-11-10 13:14:01 +0000

    Initial commit

---
+++ f0006a.py
@@ -0,0 +1,10 @@
+szám1 = input('Kérem az első számot.')
+szám2 = input('Kérem a következő számot.')
+szám1 = int(szám1)
+szám2 = int(szám2)
+if szám1 <= szám2:
+  kisebb = szám1 
+else:
+  kisebb = szám2 
+print(kisebb)
+
---
+++ main.py
@@ -0,0 +1,1 @@
+import f0006a
